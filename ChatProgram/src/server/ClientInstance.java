61ec30a3f8ab99c2653414acb58e7f5df44930ea
KristoferSvensson <kristofer87sv@gmail.com>
2018-06-07 23:51:44 +0200

    Optimization & GUI-info

--- ChatProgram/src/server/ClientInstance.java
+++ ChatProgram/src/server/ClientInstance.java
@@ -23,86 +23,88 @@ import javax.swing.JOptionPane;
  */
 class ClientInstance extends Thread {
 
-	private Socket socket;
-	private String clientName;
-	private ObjectOutputStream oos;
-	private ObjectInputStream ois;
-	private Message message;
-	private Server server;
+    private Socket socket;
+    private String clientName;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    private Message message;
+    private Server server;
 
-	public ClientInstance(String clientName, Socket socket, ObjectInputStream ois, ObjectOutputStream oos,
-			Server server) {
-		this.socket = socket;
-		this.clientName = clientName;
-		this.server = server;
-		try {
-			this.ois = ois;
-			this.oos = oos;
-			// oos = new ObjectOutputStream(socket.getOutputStream());
-			// ois = new ObjectInputStream(socket.getInputStream());
-		} catch (Exception e) {
-		}
-		checkQueuedMessages();
-		start();
-	}
+    public ClientInstance(String clientName, Socket socket, ObjectInputStream ois, ObjectOutputStream oos,
+                          Server server) {
+        this.socket = socket;
+        this.clientName = clientName;
+        this.server = server;
+        try {
+            this.ois = ois;
+            this.oos = oos;
+            // oos = new ObjectOutputStream(socket.getOutputStream());
+            // ois = new ObjectInputStream(socket.getInputStream());
+        } catch (Exception e) {
+        }
+        checkQueuedMessages();
+        start();
+    }
 
-	public void checkQueuedMessages() {
-		HashMap<String, ArrayList<Message>> map = server.getQueuedMessages();
-		if (map.containsKey(clientName)) {
-			ArrayList<Message> temp = map.get(clientName);
-			for(Message m : temp) {
-				writeMessage(m);
-			}
-		}
-	}
+    public void checkQueuedMessages() {
+        HashMap<String, ArrayList<Message>> queueMap = server.getQueuedMessages();
+        if (queueMap.containsKey(clientName)) {
+            ArrayList<String> client = new ArrayList<>();
+            client.add(clientName);
+            writeMessage(new Message(clientName, "Welcome back. You received one or more direct messages while you were offline:\n", client, 1));
+            ArrayList<Message> tempQueue = queueMap.get(clientName);
+            for (Message m : tempQueue) {
+                writeMessage(m);
+            }
+        }
+    }
 
-	public void run() {
+    public void run() {
 
-		while (!socket.isClosed()) {
-			try {
-				message = (Message) ois.readObject();
-				switch (message.getType()) {
-				// case 1 är standard textmeddelande.
-				case 1:
-					System.out.println("ClientInstance tog emot msg typ 1");
-					server.broadcastMessage(message);
-					break;
-				// case 2 är bildmeddelande.
-				case 2:
-					server.broadcastMessage(message);
-					break;
-				default:
-					break;
-				}
-			} catch (Exception e) {
-				try {
-					socket.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}
-		}
-		System.out.println(clientName + " DISCONNECTED");
-		server.broadcastDisconnectedClient(clientName);
-	}
+        while (!socket.isClosed()) {
+            try {
+                message = (Message) ois.readObject();
+                switch (message.getType()) {
+                    // case 1 är standard textmeddelande.
+                    case 1:
+                        System.out.println("ClientInstance tog emot msg typ 1");
+                        server.broadcastMessage(message);
+                        break;
+                    // case 2 är bildmeddelande.
+                    case 2:
+                        server.broadcastMessage(message);
+                        break;
+                    default:
+                        break;
+                }
+            } catch (Exception e) {
+                try {
+                    socket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        System.out.println(clientName + " DISCONNECTED");
+        server.broadcastDisconnectedClient(clientName);
+    }
 
-	/**
-	 * Skickar ett meddelande till den klient som är bunden till tråden.
-	 *
-	 * @param msg
-	 *            Meddelandet som ska skickas.
-	 */
-	public void writeMessage(Message msg) {
-		try {
-			oos.writeObject(msg);
-			oos.flush();
-			oos.reset();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Skickar ett meddelande till den klient som är bunden till tråden.
+     *
+     * @param msg Meddelandet som ska skickas.
+     */
+    public void writeMessage(Message msg) {
+        try {
+            oos.writeObject(msg);
+            oos.flush();
+            oos.reset();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String getClientName() {
-		return clientName;
-	}
+    public String getClientName() {
+        return clientName;
+    }
 }
--- ChatProgram/src/server/Server.java
+++ ChatProgram/src/server/Server.java
@@ -27,170 +27,161 @@ import java.util.logging.SimpleFormatter;
  */
 public class Server {
 
-	private int port;
-	private ServerGUI serverGUI;
-	private Clients clients;
-	// private ArrayList<ClientInstance> clientList;
-	// private ArrayList<String> clientNameList;
-	private Message message;
-	private HashMap<String, ArrayList<Message>> queuedMessages;
+    private int port;
+    private ServerGUI serverGUI;
+    private Clients clients;
+    // private ArrayList<ClientInstance> clientList;
+    // private ArrayList<String> clientNameList;
+    private Message message;
+    private HashMap<String, ArrayList<Message>> queuedMessages;
 
-	public Server(int port) {
-		serverGUI = new ServerGUI(this);
-		clients = new Clients();
-		showServerGUI();
-		this.port = port;
-		System.out.println("Server is now online.");
-		queuedMessages = new HashMap<>();
-		ConnectionListener cl = new ConnectionListener();
-		File dir = new File("tmp/test");
-		dir.mkdirs();
-		File tmp = new File(dir, "MyLogFile.log");
-		try {
-			tmp.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public Server(int port) {
+        serverGUI = new ServerGUI(this);
+        clients = new Clients();
+        showServerGUI();
+        this.port = port;
+        System.out.println("Server is now online.");
+        queuedMessages = new HashMap<>();
+        ConnectionListener cl = new ConnectionListener();
+        File dir = new File("tmp/test");
+        dir.mkdirs();
+        File tmp = new File(dir, "MyLogFile.log");
+        try {
+            tmp.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		// clientList = new ArrayList<ClientInstance>();
-		// clientNameList = new ArrayList<String>();
-		cl.start();
-	}
+        // clientList = new ArrayList<ClientInstance>();
+        // clientNameList = new ArrayList<String>();
+        cl.start();
+    }
 
-	public void showServerGUI() {
-		JFrame frame = new JFrame("Server");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(serverGUI);
-		frame.pack();
-		frame.setVisible(true);
-	}
+    public void showServerGUI() {
+        JFrame frame = new JFrame("Server");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(serverGUI);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
-	public void addToLog(String event) {
-		Logger logger = Logger.getLogger("MyLog");
-		FileHandler fh;
-		try {
-			// This block configure the logger with handler and formatter
-			// fh = new FileHandler("C:/tempLOGCHAT/MyLogFile.log", true);
-			fh = new FileHandler("tmp/test//MyLogFile.log", true);
-			logger.addHandler(fh);
-			SimpleFormatter formatter = new SimpleFormatter();
-			fh.setFormatter(formatter);
-			// the following statement is used to log any messages
-			logger.info(event);
-			fh.close();
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public void addToLog(String event) {
+        Logger logger = Logger.getLogger("MyLog");
+        FileHandler fh;
+        try {
+            // This block configure the logger with handler and formatter
+            // fh = new FileHandler("C:/tempLOGCHAT/MyLogFile.log", true);
+            fh = new FileHandler("tmp/test//MyLogFile.log", true);
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            // the following statement is used to log any messages
+            logger.info(event);
+            fh.close();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Skickar argumentet Message till alla anslutna klienter.
-	 *
-	 * @param msg
-	 *            Meddelandet som ska sändas till alla klienter.
-	 */
-	public synchronized void broadcastMessage(Message msg) { // synchronized?
+    /**
+     * Skickar argumentet Message till alla anslutna klienter.
+     *
+     * @param msg Meddelandet som ska sändas till alla klienter.
+     */
+    public synchronized void broadcastMessage(Message msg) { // synchronized?
 
-		// ArrayList<String> recipients = msg.getRecipients();
-		// String sender = recipients.get(recipients.size()-1);
-		queueMessage(msg);
-		if (msg.getType() == 1) {
-			addToLog(msg.getMsg());
-		} else if (msg.getType() == 2) {
-			// System.out.println(msg.getImage().getDescription());
-			addToLog("User sent an image file: " + msg.getImage().getDescription());
-		}
-		if (msg.getRecipients().get(0).equals("all")) {
-			for (String key : clients.getKeys()) {
-				System.out.println("Skrev msg till user:" + key);
-				clients.get(key).writeMessage(msg);
-			}
-		} else {
-			for (String key : clients.getKeys()) {
-				for (String rec : msg.getRecipients()) {
-					if (key.equals(rec)) {
-						clients.get(key).writeMessage(msg);
-					}
-				}
-			}
-		}
-	}
+        if (msg.getType() == 1) {
+            addToLog(msg.getMsg());
+        } else if (msg.getType() == 2) {
+            addToLog(msg.getUserName() + " sent an image file: " + msg.getImage().getDescription());
+        }
+        if (msg.getRecipients().get(0).equals("all")) {
+            for (String key : clients.getKeys()) {
+                clients.get(key).writeMessage(msg);
+            }
+        } else {
+            for (String recipient : msg.getRecipients()) {
+                if (clients.getKeys().contains(recipient)) {
+                    clients.get(recipient).writeMessage(msg);
+                } else {
+                    queueMessage(msg, recipient);
+                    ArrayList<String> sender = new ArrayList<>();
+                    sender.add(msg.getUserName());
+                    clients.get(msg.getUserName()).writeMessage(new Message(msg.getUserName(), "That user is not online. Your message will be delivered to them as soon as they log in\n", sender, 1));
+                }
+            }
+        }
+    }
 
-	public synchronized void broadcastDisconnectedClient(String clientName) {
-		clients.removeClient(clientName);
-		ArrayList<String> remainingClients = new ArrayList<>();
-		remainingClients.addAll(clients.getKeys());
-		for (String key : clients.getKeys()) {
-			clients.get(key)
-					.writeMessage(new Message(clientName, clientName + " Disconnected.\n", remainingClients, 1));
-			clients.get(key).writeMessage(new Message(remainingClients));
-		}
-		addToLog(clientName + " disconnected.");
-	}
+    public synchronized void broadcastDisconnectedClient(String clientName) {
+        clients.removeClient(clientName);
+        ArrayList<String> remainingClients = new ArrayList<>();
+        remainingClients.addAll(clients.getKeys());
+        for (String key : clients.getKeys()) {
+            clients.get(key)
+                    .writeMessage(new Message(clientName, clientName + " Disconnected.\n", remainingClients, 1));
+            clients.get(key).writeMessage(new Message(remainingClients));
+        }
+        addToLog(clientName + " disconnected.");
+    }
 
-	public void queueMessage(Message msg) {
-		ArrayList<String> recipients = msg.getRecipients();
-		Set<String> connectedClients = clients.getKeys();
-		ArrayList<Message> messageQueue;
-		for (String client : recipients){
-			if (!connectedClients.contains(client)){
-				if (queuedMessages.containsKey(client)){
-					messageQueue = queuedMessages.remove(client);
-					messageQueue.add(msg);
-					queuedMessages.put(client, messageQueue);
-				} else {
-					messageQueue = new ArrayList<>();
-				messageQueue.add(msg);
-				queuedMessages.put(client, messageQueue);
-				}
-			}
-		}
-	}
+    public void queueMessage(Message msg, String client) {
+        ArrayList<Message> messageQueue;
+        if (queuedMessages.containsKey(client)) {
+            messageQueue = queuedMessages.remove(client);
+            messageQueue.add(msg);
+            queuedMessages.put(client, messageQueue);
+        } else {
+            messageQueue = new ArrayList<>();
+            messageQueue.add(msg);
+            queuedMessages.put(client, messageQueue);
+        }
+    }
 
-	public HashMap<String, ArrayList<Message>> getQueuedMessages() {
-		return queuedMessages;
-	}
+    public HashMap<String, ArrayList<Message>> getQueuedMessages() {
+        return queuedMessages;
+    }
 
-	/**
-	 * Lyssnar efter nya anslutningar. När en klient ansluter binds den till en
-	 * socket och en instans av klienten skapas och läggs in i en ArrayList.
-	 * Klientnamnet läggs in i motsvarande ArrayList som skickas ut till alla
-	 * anslutna klienter.
-	 *
-	 * @author Kristofer Svensson
-	 */
-	private class ConnectionListener extends Thread {
-		public void run() {
-			// System.out.println("Server is now ready to accept connections.");
-			ArrayList<String> connectedClients = new ArrayList<>();
-			try {
-				ServerSocket serverSocket = new ServerSocket(port);
-				while (true) {
-					Socket clientSocket = serverSocket.accept();
-					ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
-					ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
-					String clientName = ois.readUTF();
-					// System.out.println("Namn mottaget: " + clientName);
-					clients.addClient(new ClientInstance(clientName, clientSocket, ois, oos, Server.this));
-					connectedClients.clear();
-					connectedClients.addAll(clients.getKeys());
-					for (String key : clients.getKeys()) {
-						clients.get(key).writeMessage(
-								new Message(clientName, clientName + " connected \n", connectedClients, 1));
-						clients.get(key).writeMessage(new Message(connectedClients));
-						System.out.println("From client list: " + key);
-					}
-					addToLog(clientName + " connected.");
-				}
-			} catch (Exception e) {
-			}
-		}
+    /**
+     * Lyssnar efter nya anslutningar. När en klient ansluter binds den till en
+     * socket och en instans av klienten skapas och läggs in i en ArrayList.
+     * Klientnamnet läggs in i motsvarande ArrayList som skickas ut till alla
+     * anslutna klienter.
+     *
+     * @author Kristofer Svensson
+     */
+    private class ConnectionListener extends Thread {
+        public void run() {
+            // System.out.println("Server is now ready to accept connections.");
+            ArrayList<String> connectedClients = new ArrayList<>();
+            try {
+                ServerSocket serverSocket = new ServerSocket(port);
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+                    ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+                    String clientName = ois.readUTF();
+                    // System.out.println("Namn mottaget: " + clientName);
+                    clients.addClient(new ClientInstance(clientName, clientSocket, ois, oos, Server.this));
+                    connectedClients.clear();
+                    connectedClients.addAll(clients.getKeys());
+                    for (String key : clients.getKeys()) {
+                        clients.get(key).writeMessage(
+                                new Message(clientName, clientName + " connected. \n", connectedClients, 1));
+                        clients.get(key).writeMessage(new Message(connectedClients));
+                        //System.out.println("From client list: " + key);
+                    }
+                    addToLog(clientName + " connected.");
+                }
+            } catch (Exception e) {
+            }
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
-		Server server = new Server(1337);
-	}
+    public static void main(String[] args) {
+        Server server = new Server(1337);
+    }
 }
